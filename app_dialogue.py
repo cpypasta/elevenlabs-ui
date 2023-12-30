4b5befffae28454e143468d9076d91dea2509c4f
Brian Roller <brianroller@brians-mbp.lan>
2023-12-29 21:05:42 -0600

    saves

--- app_dialogue.py
+++ app_dialogue.py
@@ -1,4 +1,4 @@
-import os, glob
+import os, glob, json, re
 import matplotlib.pyplot as plt
 import streamlit as st
 import elevenlabs as el
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 from dotenv import load_dotenv
 from pydub import AudioSegment as seg
-from dialogues import Character, Dialogue, saved
+from dialogues import Character, Dialogue, saved, load_saved_dialogues
 
 load_dotenv()
 plt.style.use('dark_background')
@@ -21,7 +21,14 @@ def get_voices() -> list[el.Voice]:
 def get_models() -> list[el.Model]:
   return list(el.Models.from_api())
 
+def extract_name(s: str) -> str:
+  match = re.match(r"(.*?)( \(cloned\))?$", s)
+  if match:
+    return match.group(1)
+  return s  
+
 def get_voice_id(voice_name: str, voices: list[el.Voice]) -> str:
+  voice_name = extract_name(voice_name)
   voice_index = next((i for i, v in enumerate(voices) if v.name == voice_name), None)
   if voice_index:
     return voices[voice_index].voice_id
@@ -78,8 +85,39 @@ def generate_and_save_audio(
     f.write(audio)  
   return audio_file
 
+def debug_df(df: pd.DataFrame) -> None:
+  if df.empty:
+    print("dataframe is empty")
+  else:
+    print(df)
+
+def on_load_saved() -> None:
+  if "audio_files" in st.session_state:
+    del st.session_state["audio_files"]
+  if "final_audio" in st.session_state:
+    del st.session_state["final_audio"]
+    
+def load_dialogues_with_names() -> (list[str], dict):
+  saved_dialogues = load_saved_dialogues()
+  saved_names = list(saved_dialogues.keys())  
+  return saved_names, saved_dialogues
+
+def generate_dialogue_details(character_table: pd.DataFrame, dialogue_table: pd.DataFrame, voices: list[el.Voice]) -> dict:
+  characters = []
+  for i, c in character_table.iterrows():
+    characters.append(Character(c["Name"], c["Voice"], get_voice_id(c["Voice"], voices)))
+  dialogue = []
+  for i, d in dialogue_table.iterrows():
+    character = next((c for c in characters if c.name == d["Speaker"]), None)
+    dialogue.append(Dialogue(character, i, d["Text"]).to_dict()) 
+  dialogue_details = {
+    "characters": [c.to_dict() for c in characters],
+    "dialogue": dialogue
+  }  
+  return dialogue_details  
 
 if __name__ == "__main__":
+  st.set_page_config(layout="wide")
   st.title("🎧 ElevenLabs Dialogue")
   
   with st.sidebar:
@@ -95,7 +133,7 @@ if __name__ == "__main__":
         model_id = model_ids[model_index]
     
       el_voices = get_voices()
-      el_voice_names = [voice.name for voice in el_voices]
+      el_voice_names = [f"{voice.name}{' (cloned)' if voice.category == 'cloned' else ''}" for voice in el_voices]
       el_voice = st.selectbox("Voice", el_voice_names)
       if el_voice:
         el_voice_index = el_voice_names.index(el_voice)
@@ -106,8 +144,7 @@ if __name__ == "__main__":
         if el_voice_details.preview_url:
           st.audio(el_voice_details.preview_url, format="audio/mp3")
           
-      with st.expander("Voice Options"):      
-        st.markdown(f"_ID: {el_voice_id}_")        
+      with st.expander("Voice Options"):                     
         stability = st.slider(
           "Stability", 
           0.0, 
@@ -137,24 +174,62 @@ if __name__ == "__main__":
           value=200,
           help="The gap between spoken lines in milliseconds."
         )
+        
+      with st.expander("Debug"):
+        st.markdown(f"_Model ID: {model_id}_")
+        st.markdown(f"_Voice ID: {el_voice_id}_") 
   
-  saved_dialogues = list(saved.keys())
-  saved_dialogue = st.selectbox("Load Saved Dialogues", saved_dialogues, index=None)
+  with st.expander("Saved Dialogues"):
+    col1, col2 = st.columns([7, 4])
+    with col1:
+      save_filename = st.text_input("filename", label_visibility="collapsed", placeholder="Dialogue Name")
+    with col2:
+      col21, col22 = st.columns([1, 1])
+      with col21:
+        save_dialog = st.button("Save Dialogue", use_container_width=True)
+      with col22:
+        show_json= st.button("Show JSON", use_container_width=True)
+    
+    saved_names, saved_dialogues = load_dialogues_with_names()
+    col1, col2 = st.columns([7, 4])
+    with col1:
+      saved_dialogue = st.selectbox(
+        "Loads", 
+        saved_names, 
+        index=None, 
+        on_change=on_load_saved, 
+        placeholder="Select a dialogue to load",
+        label_visibility="collapsed"
+      )
+    with col2:
+      col21, col22 = st.columns([1, 1])
+      with col21:
+        refresh_dialogues = st.button("Refresh", use_container_width=True)
+        if refresh_dialogues:
+          pass # forces rerun
+      with col22:
+        delete_dialogoue = st.button("Delete", use_container_width=True)
+        if delete_dialogoue and saved_dialogue:
+          saved_path = f"./saves/{saved_dialogue}.json"
+          if os.path.exists(saved_path):
+            os.remove(saved_path)
+            st.toast("Dialogue has been deleted. You will have to click refresh to see it.", icon="👍")
   
   st.header("Characters")
   st.markdown("This is where you setup and define what characters are in your dialogue along with what voice the character should use. You can use the sidebar if you want to hear what a voice sounds like.")
-  st.info("Adding or changing a name will clear the dialogue.")
+  st.info("Modifying names will clear the dialogue. You can freely change the voice however.")
     
   if el_key:
     if saved_dialogue:
-      character_data = [c.to_dict() for c in saved[saved_dialogue]["characters"]]
+      character_data = [c.to_dict() for c in saved_dialogues[saved_dialogue]["characters"]]
     else:
-      character_data = []
+      character_data = []    
     character_table = st.data_editor(
       pd.DataFrame(character_data, columns=["Name", "Voice"]),
       use_container_width=True,
-      hide_index=True, 
+        hide_index=True,
       num_rows="dynamic",
+      key="character_table",
       column_config={
         "Name": st.column_config.TextColumn(
           "Name",
@@ -177,14 +252,33 @@ if __name__ == "__main__":
     
     st.header("Dialogue")
     st.markdown("This is where you write the dialogue. The audio dialogue will use the model and voice settings defined in the sidebar. You can generate the audio multiple times, so feel free to generate whenever you want to hear your progress.")
+    
     if saved_dialogue:
-      dialogue_data = [d.to_dict() for d in saved[saved_dialogue]["dialogue"]]
+      characters_changes = st.session_state["character_table"]
+      edited_characters = False
+      if "edited_rows" in characters_changes:
+        edited_rows = characters_changes["edited_rows"]
+        edited_cnt = 0
+        for r_key in edited_rows.keys():
+          change = edited_rows[r_key]
+          if "Name" in change.keys():
+            edited_characters = True
+            break
+      removed_characters = "deleted_rows" in characters_changes and len(characters_changes["deleted_rows"]) > 0
+      if edited_characters or removed_characters:
+        dialogue_df = pd.DataFrame([], columns=["Speaker", "Text"])
+      else:
+        dialogue_data = [d.to_dict(without_line=True) for d in saved_dialogues[saved_dialogue]["dialogue"]]
+        dialogue_df = pd.DataFrame(dialogue_data, columns=["Speaker", "Text"])
     else:
-      dialogue_data = []    
+      dialogue_df = pd.DataFrame([], columns=["Speaker", "Text"])
+    
     dialogue_table = st.data_editor(
-      pd.DataFrame(dialogue_data, columns=["Speaker", "Text"]),
+      dialogue_df,
       use_container_width=True,
       num_rows="dynamic",
+      hide_index=True,
+      key="dialogue_table",
       column_config={
         "Speaker": st.column_config.SelectboxColumn(
           "Speaker",
@@ -198,17 +292,35 @@ if __name__ == "__main__":
           width="large"
         )
       }
-    )
+    )    
+    
+    # save dialogue to file
+    if save_dialog and save_filename:
+      if character_table.empty or dialogue_table.empty:
+        st.toast("Please enter characters and dialogue before saving.", icon="🚨")
+      else:
+        dialogue_details = generate_dialogue_details(character_table, dialogue_table, el_voices)      
+        with open(f"./saves/{save_filename}.json", "w") as f:
+          json.dump(dialogue_details, f, indent=2)
+        st.toast("Dialogue has been saved. You will have to click refresh to see it.", icon="👍")
+    if show_json and not character_table.empty and not dialogue_table.empty:
+      with st.expander("JSON", expanded=True):
+        st.toast("The JSON is shown below. Please scroll down to see it.", icon="👍")
+        st.json(generate_dialogue_details(character_table, dialogue_table, el_voices))
     
     if not dialogue_table.empty:
       dialogue = []
       for i, row in dialogue_table.iterrows():
-        character_index = character_names.index(row["Speaker"])
-        character = characters[character_index]
-        dialogue.append(Dialogue(character, i, row["Text"]))
+        try:
+          character_index = character_names.index(row["Speaker"])
+          character = characters[character_index]
+          dialogue.append(Dialogue(character, i, row["Text"]))
+        except:
+          print(f"Error: {row['Speaker']} is not a valid character.")
+          pass        
       
       # generate audio dialogue
-      generate_btn = st.button("Generate Dialogue")      
+      generate_btn = st.button("Generate Audio Dialogue")      
       if generate_btn:
         st.session_state["final_audio"] = False
         for file in glob.glob("./audio/*.mp3"):
@@ -217,13 +329,26 @@ if __name__ == "__main__":
           os.mkdir("./audio")
         audio_files = []
         progress_text = "Generating audio..."
+        if "audio_process_error" in st.session_state:
+          del st.session_state["audio_process_error"]
         generate_audio_bar = st.progress(0, text=progress_text)
         for i, line in enumerate(dialogue):
-          audio_file = generate_and_save_audio(line, model_id, stability, simarlity_boost, style) 
-          audio_files.append(audio_file)
-          generate_audio_bar.progress(round((i+1) / len(dialogue), 2), text=progress_text)
+          try:
+            audio_file = generate_and_save_audio(line, model_id, stability, simarlity_boost, style) 
+            audio_files.append(audio_file)
+          except Exception as e:
+            print(e)
+            st.session_state["audio_process_error"] = f"{dialogue.character.name} with the voice {dialogue.character.voice} (voice_id: {dialogue.character.voice_id})"
+            break
+          generate_audio_bar.progress(round((i+1) / len(dialogue), 2), text=progress_text)          
         generate_audio_bar.empty()
-        st.session_state["audio_files"] = audio_files
+        
+        if "audio_process_error" in st.session_state:
+          st.error(f"""An error occured while generating the audio. Please check your API key.
+          Error occurred while processing: {st.session_state.audio_process_error}
+          """)
+        else:
+          st.session_state["audio_files"] = audio_files
       
       if "audio_files" in st.session_state:        
         # display generated audio
--- dialogues.py
+++ dialogues.py
@@ -1,4 +1,5 @@
-import elevenlabs as el
+import json
+from pathlib import Path
 
 class Character:
   def __init__(self, name: str, voice: str, voice_id: str) -> None:
@@ -9,11 +10,15 @@ class Character:
   def to_dict(self) -> dict:
     return {
       "Name": self.name,
-      "Voice": self.voice
+      "Voice": self.voice,
+      "Voice_ID": self.voice_id
     }
   
   def __str__(self):
-    return f"{self.name} ({self.voice})"
+    return f"{self.name};{self.voice};{self.voice_id}"
+  
+  def __repr__(self) -> str:
+    return self.__str__()
 
 
 class Dialogue:
@@ -22,21 +27,44 @@ class Dialogue:
     self.line = line
     self.text = text
   
-  def to_dict(self) -> dict:
-    return {
-      "Speaker": self.character.name,
-      "Text": self.text
-    }
+  def to_dict(self, without_line: bool = False) -> dict:
+    if without_line:
+      return {
+        "Speaker": self.character.name,
+        "Text": self.text
+      }
+    else:
+      return {
+        "Speaker": self.character.name,
+        "Line": self.line,
+        "Text": self.text
+      }
     
   def __str__(self):
     return f"[{self.line}] {self.character.name}: {self.text}"
 
 
+def load_saved_dialogues() -> dict:
+  directory = Path("./saves")
+  names = {}
+  for json_file in directory.glob("*.json"):
+    characters = []
+    dialogues = []
+    with open(json_file, "r") as f:
+      data = json.load(f)
+      for character in data["characters"]:
+        characters.append(Character(character["Name"], character["Voice"], character["Voice_ID"]))
+      for dialogue in data["dialogue"]:
+        character = next((c for c in characters if c.name == dialogue["Speaker"]), None)
+        dialogues.append(Dialogue(character, dialogue["Line"], dialogue["Text"]))
+    names[json_file.stem] = { "characters": characters, "dialogue": dialogues }
+  return names
+      
 tracy_emily = Character("Tracy", "Emily", "LcfcDJNUP1GQjkzn1xUU")
 hulk_callum = Character("Hulk", "Callum", "N2lVS1w4EtoT3dr4eOWO")
 
 saved = {
-  "Dumb Hulk": {
+  "Hulk Smash": {
     "characters": [
       tracy_emily,
       hulk_callum
@@ -44,11 +72,11 @@ saved = {
     "dialogue": [
       Dialogue(tracy_emily, 0, "Hello, who are you?"),
       Dialogue(hulk_callum, 1, "Hulk smash!"),
-      Dialogue(tracy_emily, 2, "Okay. Nice to meet you hulk."),
+      Dialogue(tracy_emily, 2, "That's nice to hear. Nice to meet you hulk."),
       Dialogue(hulk_callum, 3, "Hulk smash!"),
-      Dialogue(tracy_emily, 4, "Okay. I get it. You smash things."),
+      Dialogue(tracy_emily, 4, "So you say. You smash things."),
       Dialogue(hulk_callum, 5, "Hulk smash!"),
-      Dialogue(tracy_emily, 6, "Okay. I'm going to leave now.")
+      Dialogue(tracy_emily, 6, "Very nice. I'm going to leave now.")
     ]
   }
 }
